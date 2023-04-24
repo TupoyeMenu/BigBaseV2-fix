94a371f7866382a857d0c5abdddd6140d06caef5
tupoy-ya <tupoy-ya@users.noreply.github.com>
2023-04-24 21:31:18 +0500

    refactor(g3log): Use upstream g3log. chore(Vehicle values)!: Removed `VehicleWeapons` "enum". chore(natives): Updated natives. chore: Remove `gta/matrix.hpp`.

--- cmake/g3log.cmake
+++ cmake/g3log.cmake
@@ -7,8 +7,8 @@ set(G3_SHARED_RUNTIME OFF CACHE INTERNAL "")
 
 FetchContent_Declare(
     g3log
-    GIT_REPOSITORY https://github.com/YimMenu/g3log.git
-    GIT_TAG        346502dd578c7b885970fb8a6ff4ceabae69fb35
+    GIT_REPOSITORY https://github.com/KjellKod/g3log.git
+    GIT_TAG        b249fb6c6cb5c6badb19d2ccece730dd0278ac6b
     GIT_PROGRESS TRUE
 )
 message("g3log")
