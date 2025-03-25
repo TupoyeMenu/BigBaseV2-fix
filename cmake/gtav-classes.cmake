15b1f14b2cf1e130f3b300bf4cc4579966862281
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-03-25 21:29:52 +0500

    fix(GTAV-Classes): Change to a newer repo that exists.
    Not sure how long it will exist for.
    Untested in game.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,8 +3,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        aebd69542e58fab8975da76c3e555b122ddef5d6
+    GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
+    GIT_TAG        e803b20ec3d6cf20d6b254c915e088bd010f006e
     GIT_PROGRESS TRUE
     SOURCE_SUBDIR "why" # FetchContent_Populate is depricated now, so we set this to a non existing directory.
 )
